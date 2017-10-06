68da491ef8eea39317b6b905b714b4cf524b6f1e
ponychan <rzsckbmx@163.com>
2017-10-06 11:53:20 +0800

    3.7.cpp
    嵌套调用

--- ConsoleApplication3_7/ConsoleApplication37.cpp
+++ ConsoleApplication3_7/ConsoleApplication37.cpp
@@ -1,11 +1,26 @@
 // ConsoleApplication37.cpp: 定义控制台应用程序的入口点。
-//
+//3.7.cpp嵌套调用求平方和
 
 #include "stdafx.h"
+#include<iostream>
+using namespace std;
 
+//求平方的函数
+int fun2(int m) {
+	return m*m;
+}
+
+//求和的函数
+int fun1(int x, int y) {
+	return fun2(x) + fun2(y);
+}
 
 int main()
 {
-    return 0;
+	int a, b;
+	cout << "please enter two integers(a and b):";
+	cin >> a >> b;
+	cout << "the sum of square of a and b:" << fun1(a, b) << endl;
+	return 0;
 }
 
